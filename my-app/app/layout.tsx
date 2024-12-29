ddaf5ceb2222dfb965b151b29ed5bd87a3abe12f
Devrat <devratpatel1@gmail.com>
2024-12-29 00:24:45 -0600

    update: metadata

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/devratmeta.png",
+        url: "/devratmeta.jpg",
         width: 1200,
         height: 630,
         alt: "Devrat Patel",
