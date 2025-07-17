ce5e241711f17fb3d609c3180feb222e7e2b6e35
Devrat <devratpatel1@gmail.com>
2025-07-16 20:01:25 -0500

    faivcon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,13 @@ export const metadata: Metadata = {
   viewport:
     "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover",
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
   keywords: [
     "Devrat",
